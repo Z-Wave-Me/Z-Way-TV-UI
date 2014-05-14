d634a21f3131da737c595b3240a2d89b399e816e
Stanislav Morozov <r3b@seoarmy.ru>
2014-05-14 17:35:01 +0700

    Add less and update focus

--- js/views/devicesView.js
+++ js/views/devicesView.js
@@ -21,10 +21,36 @@
         },
 
         renderWidget: function (model) {
-            var that = this;
+            var that = this,
+                view = model.hasOwnProperty('view') ? model.view : null;
 
-            if (model.get('deviceType') === 'probe') {
+            if (view === null) {
+                if (model.get('deviceType') === "sensorBinary" || model.get('deviceType') === "sensorMultilevel" || model.get('deviceType') === "battery") {
+                    view = new window.App.views.probe({model: model});
+                } else if (model.get('deviceType') === "fan") {
 
+                } else if (model.get('deviceType') === "switchMultilevel") {
+
+                } else if (model.get('deviceType') === "thermostat") {
+
+                } else if (model.get('deviceType') === "doorlock") {
+
+                } else if (model.get('deviceType') === "switchBinary" || model.get('deviceType') === "switchRGBW") {
+
+                } else if (model.get('deviceType') === "toggleButton") {
+
+                } else if (model.get('deviceType') === "camera") {
+
+                } else if (model.get('deviceType') === "switchControl") {
+
+                } else {
+                    //log(model);
+                }
+            }
+
+
+            if (view !== null) {
+                view.render();
             }
         },
 
--- js/views/probeView.js
+++ js/views/probeView.js
@@ -1,21 +1,28 @@
 (function () {
     "use strict";
 
-    window.App.views.devices = Backbone.View.extend({
+    window.App.views.probe = Backbone.View.extend({
 
-        el: '.js-scene-devices',
+        el: '.container-devices',
+        template:
+            '<div class="widget widget-probe nav-item just-hidden">' +
+                '<span class="icon"></span>' +
+                '<span class="title"><%= metrics.title %></span>' +
+                '<span class="level"><%= metrics.level %> <%= metrics.scaleTitle %></span>' +
+            '</div>',
 
         initialize: function () {
             _.bindAll(this, 'render');
             var that = this;
 
-            that.scene = 'rooms';
-            that.listenTo(that.collection, 'add', that.renderWidget);
+            that.$template = $(_.template(that.template, that.model.toJSON()));
             //console.log('Init model');
         },
 
         render: function () {
             var that = this;
+            that.$el.append(that.$template);
+            that.$template.fadeIn();
         }
     });
 })();
